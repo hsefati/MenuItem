5472f7b35da173b573160a2463b67c0c05750ffa
hsefati <hsefati@gmail.com>
2017-10-01 22:41:19 +0200

    Adding camera as background layout. Make menuItem transparent
    To DO:
    
    Introduce Thread to reduce the load on the main thread

---
+++ app/src/main/java/com/example/mrblue/test/CameraSurfaceView.java
@@ -0,0 +1,146 @@
+package com.example.mrblue.test;
+
+import android.content.Context;
+import android.graphics.ImageFormat;
+import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+/**
+ * Created by Mr.Blue on 7/13/2017.
+ *
+ * Camera preview class
+ */
+public class CameraSurfaceView extends SurfaceView implements SurfaceHolder.Callback{
+
+    private Camera myCamera;
+    private SurfaceHolder holder;
+    private boolean isConfigured = false;
+
+    private static final String TAG = "SimpleCameraApp";
+
+    public CameraSurfaceView(Context context, Camera myCamera) {
+        super(context);
+        this.myCamera = myCamera;
+
+
+        // Install a SurfaceHolder.Callback so we get notified when the
+        // underlying surface is created and destroyed.
+        this.holder = getHolder();
+        holder.addCallback(this);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        // The Surface has been created, now tell the camera where to draw the preview.
+
+        try {
+            isConfigured = false;
+            myCamera.setPreviewDisplay(holder);
+            myCamera.startPreview();
+        }catch (IOException e){
+            Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // If your preview can change or rotate, take care of those events here.
+        // Make sure to stop the preview before resizing or reformatting it.
+
+
+        // The section supposed to reduce the resolution of the picture, which is shown in surfaceView
+        Camera.Parameters parametersCamera =myCamera.getParameters();
+        Camera.Size size=getBestPreviewSize(width, height, parametersCamera);
+        Camera.Size pictureSize=getSmallestPictureSize(parametersCamera);
+
+        if(!isConfigured) {
+            if (size != null && pictureSize != null) {
+                parametersCamera.setPreviewSize(size.width, size.height);
+                parametersCamera.setPictureSize(pictureSize.width,
+                        pictureSize.height);
+                parametersCamera.setPictureFormat(ImageFormat.JPEG);
+                myCamera.setParameters(parametersCamera);
+                isConfigured = true;
+            }
+        }
+
+        if (holder.getSurface() == null){
+            // preview surface does not exist
+            return;
+        }
+
+        // stop preview before making changes
+        try {
+//            myCamera.stopPreview();
+        } catch (Exception e){
+            // ignore: tried to stop a non-existent preview
+        }
+
+        // set preview size and make any resize, rotate or
+        // reformatting changes here
+
+        // start preview with new settings
+        try {
+//            myCamera.setPreviewDisplay(holder);
+//            myCamera.startPreview();
+        } catch (Exception e){
+            Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // empty. Take care of releasing the Camera preview in your activity.
+
+        myCamera.stopPreview();
+        myCamera.release();
+    }
+
+    private Camera.Size getBestPreviewSize(int width, int height,
+                                           Camera.Parameters parameters) {
+        Camera.Size result=null;
+
+        for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
+            if (size.width <= width && size.height <= height) {
+                if (result == null) {
+                    result=size;
+                }
+                else {
+                    int resultArea=result.width * result.height;
+                    int newArea=size.width * size.height;
+
+                    if (newArea > resultArea) {
+                        result=size;
+                    }
+                }
+            }
+        }
+
+        return(result);
+    }
+
+    private Camera.Size getSmallestPictureSize(Camera.Parameters parameters) {
+        Camera.Size result=null;
+
+        for (Camera.Size size : parameters.getSupportedPictureSizes()) {
+            if (result == null) {
+                result=size;
+            }
+            else {
+                int resultArea=result.width * result.height;
+                int newArea=size.width * size.height;
+
+                if (newArea < resultArea) {
+                    result=size;
+                }
+            }
+        }
+
+        return(result);
+    }
+}
--- app/src/main/java/com/example/mrblue/test/MainActivity.java
+++ app/src/main/java/com/example/mrblue/test/MainActivity.java
@@ -1,53 +1,94 @@
 package com.example.mrblue.test;
 
+import android.Manifest;
+import android.content.res.Configuration;
+import android.hardware.Camera;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-
 import android.app.ListActivity;
-        import android.content.ComponentName;
-        import android.content.Intent;
-        import android.content.pm.ActivityInfo;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
         import android.content.pm.PackageManager;
         import android.content.pm.ResolveInfo;
-        import android.os.Bundle;
-        import android.view.View;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
         import android.view.ViewGroup;
         import android.widget.ArrayAdapter;
-        import android.widget.ImageView;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
         import android.widget.ListView;
         import android.widget.TextView;
         import java.util.Collections;
-        import java.util.Comparator;
         import java.util.List;
 
 public class MainActivity extends ListActivity {
     AppAdapter adapter=null;
+
+    Camera myCamera;
+    FrameLayout myLayout;
+    CameraSurfaceView CameraSurfaceView;
+
+    private boolean mIsPreviewing;
+    private static final int PREVIEW_PAUSE = 500;
+    private static final int REQUEST_CAMERA = 0;
+
+    private static final String TAG = "testProject";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PackageManager pm=getPackageManager();
-        Intent main=new Intent(Intent.ACTION_MAIN, null);
+        PackageManager packetManager=getPackageManager();
+        Intent mainIntent=new Intent(Intent.ACTION_MAIN, null);
 
-        main.addCategory(Intent.CATEGORY_LAUNCHER);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
 
-        List<ResolveInfo> launchables=pm.queryIntentActivities(main, 0);
+        List<ResolveInfo> launchableApps = packetManager.queryIntentActivities(mainIntent, 0);
 
-        Collections.sort(launchables,
-                new ResolveInfo.DisplayNameComparator(pm));
+        Collections.sort(launchableApps,
+                new ResolveInfo.DisplayNameComparator(packetManager));
 
-        adapter=new AppAdapter(pm, launchables);
+        adapter=new AppAdapter(packetManager, launchableApps);
         setListAdapter(adapter);
+
+        if(getCameraPermission()){
+            myCamera = getCameraInstance();
+        }
+
+        if(myCamera != null)
+            initAfterCameraGranted();
+    }
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+        // Release camera resources
+        releaseCameraResources();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 
     @Override
     protected void onListItemClick(ListView l, View v,
                                    int position, long id) {
-        ResolveInfo launchable=adapter.getItem(position);
-        ActivityInfo activity=launchable.activityInfo;
+        ResolveInfo launchableApps=adapter.getItem(position);
+        ActivityInfo activity=launchableApps.activityInfo;
         ComponentName name=new ComponentName(activity.applicationInfo.packageName,
                 activity.name);
         Intent i=new Intent(Intent.ACTION_MAIN);
@@ -60,11 +101,101 @@ public class MainActivity extends ListActivity {
         startActivity(i);
     }
 
+    private void initAfterCameraGranted(){
+
+        myLayout = (FrameLayout) findViewById(R.id.camera_preview);
+
+        CameraSurfaceView = new CameraSurfaceView(this, myCamera);
+        myLayout.addView(CameraSurfaceView);
+    }
+
+    public boolean getCameraPermission(){
+        // Check for the camera permission. If it is not granted to this app already, it will ask an user for the permission.
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.CAMERA}, REQUEST_CAMERA);
+
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+
+    private Camera getCameraInstance(){
+        Camera cam_obj = null;
+
+        try {
+            cam_obj = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            Log.d(TAG, "Error getting a camera instance: " + e.getMessage());
+        }
+
+        Camera.Parameters parameters = cam_obj.getParameters();
+
+        if (this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) {
+
+            parameters.set("orientation", "potrait");
+            cam_obj.setDisplayOrientation(90);
+            parameters.setRotation(90);
+        }
+        else{
+            parameters.set("orientation", "landscape");
+            cam_obj.setDisplayOrientation(0);
+            parameters.setRotation(0);
+        }
+
+        return cam_obj;
+    }
+
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        if (requestCode == REQUEST_CAMERA) {
+            // BEGIN_INCLUDE(permission_result)
+            // Received permission result for camera permission.
+            Log.i(TAG, "Received response for Camera permission request.");
+
+            // Check if the only required permission has been granted
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Camera permission has been granted, preview can be displayed
+
+                myCamera = getCameraInstance();
+                initAfterCameraGranted();
+
+                Log.i(TAG, "CAMERA permission has now been granted. Showing preview.");
+            } else {
+                Log.i(TAG, "CAMERA permission was NOT granted.");
+            }
+            // END_INCLUDE(permission_result)
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    // Release camera so other applications can use it.
+    private void releaseCameraResources() {
+        if (null != myCamera) {
+//          myCamera.release();
+            myCamera = null;
+        }
+    }
+
+
+    ///////////////////////////////////////////////////////////////
+
     class AppAdapter extends ArrayAdapter<ResolveInfo> {
         private PackageManager pm=null;
 
         AppAdapter(PackageManager pm, List<ResolveInfo> apps) {
-            super(MainActivity.this, R.layout.row, apps);
+            super(MainActivity.this.getApplicationContext(), R.layout.row, apps);
             this.pm=pm;
         }
 
